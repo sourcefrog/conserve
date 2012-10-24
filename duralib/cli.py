7d6e1b4dc10ea9b647e1fbc9a11d61a7094db14a
Martin Pool <mbp@sourcefrog.net>
2012-10-25 07:35:20 +1100

    Use cmd function help in argparse

--- duralib/cli.py
+++ duralib/cli.py
@@ -13,51 +13,67 @@ from duralib.archive import Archive
 _log = logging.getLogger('dura')
 
 
+def _first_line(s):
+    """Return the first line of s, without the newline."""
+    return s.split('\n', 1)[0]
+
+
+def _parser_for_cmd(cmd_name):
+    cmd_func = globals()['cmd_' + cmd_name.replace('-', '_')]
+    cp = _cmd_subparsers.add_parser(
+        cmd_name,
+        help=_first_line(cmd_func.__doc__),
+        description=cmd_func.__doc__,
+        )
+    cp.set_defaults(cmd_func=cmd_func)
+    return cp
+
+
 def _make_parser():
     """Make an ArgumentParser"""
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(
-        title='commands')
+    global _cmd_subparsers
+    _cmd_subparsers = parser.add_subparsers(
+        title='commands',
+        metavar='command',  # argparse arg metavars are in lowercase.
+        )
 
-    cp = subparsers.add_parser(
-        'create-archive',
-        help='Make a new archive to hold backups')
-    cp.set_defaults(cmd_func=cmd_create_archive)
+    cp = _parser_for_cmd('create-archive')
     cp.add_argument(
         'archive_directory',
         help='Local path to directory to be created')
 
-    cp = subparsers.add_parser(
-        'describe-archive',
-        help='Show summary information about an archive')
-    cp.set_defaults(cmd_func=cmd_describe_archive)
+    cp = _parser_for_cmd('describe-archive')
     cp.add_argument(
         'archive_directory',
-        help='Local path to directory to be created')
+        help='Local path to archive directory')
 
-    cp = subparsers.add_parser(
-        'dump-index',
-        help='Show debug information about index files')
-    cp.set_defaults(cmd_func=cmd_dump_index)
+    cp = _parser_for_cmd('dump-index')
     cp.add_argument(
         'index_files',
         nargs='*',
-        help='Index files to dump')
+        help='Path to a .i index file to dump')
 
     return parser
 
 
 def cmd_create_archive(args):
+    """Make a new archive to hold backups.
+
+    The destination directory will be created, and must not exist.
+    """
     new_archive = Archive.create(args.archive_directory)
     _log.info("Created %s", new_archive)
 
 
 def cmd_describe_archive(args):
+    """Show summary information about an archive."""
     archive = Archive.open(args.archive_directory)
     _log.info("Opened archive %r", archive)
 
 
 def cmd_dump_index(args):
+    """Show debug information about index files."""
     from duralib.dump import dump_index_block
     for index_file_name in args.index_files:
         dump_index_block(index_file_name)
