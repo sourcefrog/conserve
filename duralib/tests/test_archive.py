a7860b3321c6550155df82e17ffb6ac3e24aad69
Martin Pool <mbp@sourcefrog.net>
2012-10-28 07:55:09 +1100

    Rearrange imports

--- duralib/tests/test_archive.py
+++ duralib/tests/test_archive.py
@@ -14,8 +14,13 @@ import unittest
 from fixtures import TempDir, TestWithFixtures
 from testresources import ResourcedTestCase
 
-from duralib import archive
+from duralib.archive import (
+    Archive,
+    BadArchiveHeader,
+    NoSuchArchive,
+    )
 from duralib.tests.base import DuraTestCase
+from duralib.tests.fixtures import EmptyArchive
 
 
 class TestArchive(DuraTestCase):
@@ -25,7 +30,7 @@ class TestArchive(DuraTestCase):
         self.archive_path = self.subpath("testarchive")
 
     def test_create_archive(self):
-        new_archive = archive.Archive.create(self.archive_path)
+        new_archive = Archive.create(self.archive_path)
         self.assertEquals(self.archive_path, new_archive.path)
         self.assertTrue(os.path.isdir(self.archive_path))
         self.assertTrue(
@@ -33,24 +38,25 @@ class TestArchive(DuraTestCase):
                 os.path.join(self.archive_path, "DURA-ARCHIVE")))
 
     def test_reopen_archive(self):
-        new_archive = archive.Archive.create(self.archive_path)
-        second = archive.Archive.open(self.archive_path)
+        new_archive = Archive.create(self.archive_path)
+        second = Archive.open(self.archive_path)
         self.assertEquals(self.archive_path, second.path)
 
     def test_open_nonexistent(self):
         # Don't create it
-        with self.assertRaises(archive.NoSuchArchive) as ar:
-            archive.Archive.open(self.archive_path)
+        with self.assertRaises(NoSuchArchive) as ar:
+            Archive.open(self.archive_path)
         self.assertRegexpMatches(str(ar.exception),
             r"No such archive: .*testarchive.*%s"
             % os.strerror(errno.ENOENT))
 
     def test_open_bad_magic(self):
-        orig_archive = archive.Archive.create(self.archive_path)
+        orig_archive = Archive.create(self.archive_path)
         with file(orig_archive._header_path, "wb") as f:
             f.write("not this!")
-        with self.assertRaises(archive.BadArchiveHeader) as ar:
-            archive.Archive.open(self.archive_path)
+        with self.assertRaises(BadArchiveHeader) as ar:
+            Archive.open(self.archive_path)
         self.assertEquals(
             "Bad archive header: %s" % orig_archive._header_path,
             str(ar.exception))
+
