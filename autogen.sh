019feb5f63da84a51ebb3dff315dade0608da1c7
Martin Pool <mbp@sourcefrog.net>
2013-07-22 07:47:19 +1000

    autoconfiscate, sigh

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#! /bin/sh -e
+
+# 'missing' may be missing on the first run in a bare tree
+[ -f missing ] && MISSING="missing --run" || MISSING=
+
+$MISSING aclocal
+$MISSING automake --foreign --add-missing
+$MISSING autoconf
+./configure
