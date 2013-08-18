cec5d5fb63c4fc61d974f11b9e204e8b516ac77d
Martin Pool <mbp@sourcefrog.net>
2013-08-18 19:22:31 +1000

    Use 'missing' script from in the tree

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #! /bin/sh -e
 
 # 'missing' may be missing on the first run in a bare tree
-[ -f missing ] && MISSING="missing --run" || MISSING=
+[ -f missing ] && MISSING="./missing --run" || MISSING=
 
 $MISSING aclocal
 $MISSING automake --foreign --add-missing
