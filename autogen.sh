bc6b6ce53955bde085386eabf0c7978f746fba8c
Martin Pool <mbp@sourcefrog.net>
2013-12-01 21:44:50 +1100

    Convert automake to a hacky go-based Makefile

--- autogen.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh -e
-
-# 'missing' may be missing on the first run in a bare tree
-[ -f missing ] && MISSING="./missing --run" || MISSING=
-
-$MISSING aclocal
-$MISSING automake --foreign --add-missing
-$MISSING autoconf
