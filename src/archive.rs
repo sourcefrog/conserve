4a7a20b4d07d6c22ee0c9a8becc3a099182c83ea
Martin Pool <mbp@sourcefrog.net>
2015-07-26 13:38:11 -0700

    Change archive header filename back to 'CONSERVE'

--- src/archive.rs
+++ src/archive.rs
@@ -5,6 +5,7 @@ use std::path::{Path, PathBuf} ;
 
 use rustc_serialize::json;
 
+const HEADER_FILENAME: &'static str = "CONSERVE";
 const ARCHIVE_VERSION: &'static str = "0.2.0";
 
 #[derive(Debug)]
@@ -47,7 +48,7 @@ impl Archive {
         let header = ArchiveHeader{
             conserve_archive_version: String::from(ARCHIVE_VERSION),
         };
-        let header_path = self.dir.join("conserve-archive");
+        let header_path = self.dir.join(HEADER_FILENAME);
         let mut header_file = match File::create(&header_path) {
             Ok(f) => f,
             Err(e) => {
