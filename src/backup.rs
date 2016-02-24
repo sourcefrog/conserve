39f1e44cd64834fe32a4af3b9cb40c2e663817e0
Martin Pool <mbp@sourcefrog.net>
2016-02-24 06:51:25 -0800

    Further stub implementation of backup

--- src/backup.rs
+++ src/backup.rs
@@ -5,14 +5,43 @@ use std::path::{Path};
 use std::io::{Result};
 use walkdir::WalkDir;
 
+use super::Archive;
+
 
 pub fn run_backup(archive_path: &Path, sources: Vec<&Path>) -> Result<()> {
-    // TODO: Sort the results.
+    // TODO: Sort the results: probably grouping together everything in a
+    // directory, and then by file within that directory.
+    Archive::open(archive_path).unwrap();
     for source_dir in sources {
         for entry in WalkDir::new(source_dir) {
-            // TODO: Just warn if not ok.
-            info!("backup {}", entry.unwrap().path().display());
+            match entry {
+                Ok(entry) => if entry.metadata().unwrap().is_file() {
+                    backup_one(entry.path());
+                },
+                Err(e) => {
+                    warn!("{}", e);
+                }
+            }
         }
     };
     Ok(())
+}
+
+fn backup_one(path: &Path) {
+    info!("backup {}", path.display());
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate tempdir;
+    
+    use super::super::archive::scratch_archive;
+    use super::run_backup;
+    
+    #[test]
+    pub fn test_simple_backup() {
+        let (_tempdir, archive) = scratch_archive();
+        let srcdir = tempdir::TempDir::new("conserve-srcdir").unwrap();
+        run_backup(archive.path(), vec![srcdir.path()]).unwrap();
+    }
 }
