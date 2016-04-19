52753869a8cd9e82066a7fcbd3761c6f901ffeb3
Martin Pool <mbp@sourcefrog.net>
2016-04-18 22:36:25 -0700

    write_file_entire shouldn't overwrite existing files.
    By design Conserve should never overwrite or update a file in the
    archive, since there's some risk it could go wrong or be half updated.
    For hash-addressed block data, the caller will just accept the
    AlreadyExists error as indicating the value's already stored.
    
    This requires Rust >=1.9 to get the new OpenOptions.

--- src/io.rs
+++ src/io.rs
@@ -3,16 +3,20 @@
 
 //! IO utilities.
 
-use std::fs::{File, remove_file};
+use std::fs::{OpenOptions, remove_file};
 use std::io;
-use std::io::Write;
+use std::io::{Write};
 use std::path::{Path, };
 
 
-/// Write bytes to a file, and close.  If writing fails, delete the file.
+/// Write bytes to a file, and close.
+/// If writing fails, delete the file.
+/// The file must not already exist.
+///
+/// NOTE: This requires Rust >= 1.9 for `OpenOptions::create_new`.
 pub fn write_file_entire(path: &Path, bytes: &[u8]) -> io::Result<()> {
     // TODO: Somehow test the error cases.
-    let mut f = match File::create(path) {
+    let mut f = match OpenOptions::new().write(true).create_new(true).open(path) {
         Ok(f) => f,
         Err(e) => {
             error!("Couldn't create {:?}: {}", path.display(), e);
@@ -28,5 +32,30 @@ pub fn write_file_entire(path: &Path, bytes: &[u8]) -> io::Result<()> {
         Err(e)
     } else {
         Ok(())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use std::io;
+    use tempdir;
+
+    use super::write_file_entire;
+
+    #[test]
+    pub fn test_write_file_entire_repeated() {
+        let tmp = tempdir::TempDir::new("write_new_file_test").unwrap();
+        let testfile = tmp.path().join("afile");
+        write_file_entire(&testfile, "hello".as_bytes()).unwrap();
+
+        assert_eq!(write_file_entire(&testfile, "goodbye".as_bytes())
+                   .unwrap_err().kind(),
+                   io::ErrorKind::AlreadyExists);
+
+        // Should not have been overwritten.
+        assert_eq!(fs::metadata(&testfile).unwrap().len(),
+            "hello".len() as u64);
     }
 }
