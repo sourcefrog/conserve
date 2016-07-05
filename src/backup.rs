af178bb0cbe0bf5f5084a96df975a1469924033b
Martin Pool <mbp@sourcefrog.net>
2016-07-04 18:27:04 -0700

    Add stored files to the index

--- src/backup.rs
+++ src/backup.rs
@@ -9,24 +9,29 @@ use std::path::{Path};
 use std::io;
 use walkdir::WalkDir;
 
+use super::apath::apath_valid;
 use super::archive::Archive;
 use super::block::{BlockDir, BlockWriter};
+use super::index::{IndexBuilder, IndexEntry, IndexKind};
 use super::report::Report;
 
 
-pub fn run_backup(archive_path: &Path, sources: Vec<&Path>, mut report: &mut Report) -> io::Result<()> {
+pub fn run_backup(archive_path: &Path, sources: Vec<&Path>, mut report: &mut Report)
+    -> io::Result<()> {
     // TODO: Sort the results: probably grouping together everything in a
     // directory, and then by file within that directory.
     let archive = Archive::open(archive_path).unwrap();
     let band = try!(archive.create_band());
     let mut block_dir = band.block_dir();
+    let mut index_builder = band.index_builder();
     for source_dir in sources {
         // TODO: Cope if sources are files rather than directories.
         // TODO: Backup directories too.
         for entry in WalkDir::new(source_dir) {
             match entry {
                 Ok(entry) => if entry.metadata().unwrap().is_file() {
-                    try!(backup_one_file(&mut block_dir, entry.path(), &mut report));
+                    try!(backup_one_file(&mut block_dir, &mut index_builder,
+                            entry.path(), &mut report));
                 },
                 Err(e) => {
                     warn!("{}", e);
@@ -34,11 +39,13 @@ pub fn run_backup(archive_path: &Path, sources: Vec<&Path>, mut report: &mut Rep
             }
         }
     };
+    try!(index_builder.finish_hunk(&mut report));
     // TODO: Mark band complete.
     Ok(())
 }
 
-fn backup_one_file(block_dir: &BlockDir, path: &Path, mut report: &mut Report) -> io::Result<()> {
+fn backup_one_file(block_dir: &BlockDir, index_builder: &mut IndexBuilder,
+    path: &Path, mut report: &mut Report) -> io::Result<()> {
     info!("backup {}", path.display());
     let mut bw = BlockWriter::new();
     let mut f = try!(fs::File::open(&path));
@@ -46,6 +53,21 @@ fn backup_one_file(block_dir: &BlockDir, path: &Path, mut report: &mut Report) -
     let _hash = try!(block_dir.store(bw, &mut report));
     // TODO: Add to the index too.  Get the hash and length from the writer.
     report.increment("backup.file.count", 1);
+
+    // TODO: Get the whole path relative to the top level source directory.
+    let apath = path.file_name().unwrap().to_str().unwrap();
+    assert!(apath_valid(apath));
+
+    // TODO: Get mtime.
+
+    let index_entry = IndexEntry {
+        apath: apath.to_string(),
+        mtime: 0,
+        kind: IndexKind::File,
+        blake2b: String::new(),
+    };
+    index_builder.push(index_entry);
+
     Ok(())
 }
 
