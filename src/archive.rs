12f6c11abc82bd330f4d9de8c00baead577d9e62
Martin Pool <mbp@sourcefrog.net>
2016-07-05 11:23:06 -0700

    Add Archive::list_bands
    Rename Archive.dir to `path` for consistency.
    
    Consolidate tests.

--- src/archive.rs
+++ src/archive.rs
@@ -8,7 +8,7 @@
 //! Archives can contain a tree of bands, which themselves contain file versions.
 
 use std;
-use std::fs::{File};
+use std::fs::{File, read_dir};
 use std::io;
 use std::io::{Error, ErrorKind, Result, Read};
 use std::path::{Path, PathBuf} ;
@@ -25,7 +25,7 @@ const ARCHIVE_VERSION: &'static str = "0.2.0";
 #[derive(Debug)]
 pub struct Archive {
     /// Top-level directory for the archive.
-    dir: PathBuf,
+    path: PathBuf,
 }
 
 #[derive(Debug, RustcDecodable, RustcEncodable)]
@@ -35,32 +35,32 @@ struct ArchiveHeader {
 
 impl Archive {
     /// Make a new directory to hold an archive, and write the header.
-    pub fn init(dir: &Path) -> Result<Archive> {
-        debug!("Creating archive directory {:?}", dir.display());
+    pub fn init(path: &Path) -> Result<Archive> {
+        debug!("Creating archive directory {:?}", path.display());
         let archive = Archive {
-            dir: dir.to_path_buf(),
+            path: path.to_path_buf(),
         };
-        if let Err(e) = std::fs::create_dir(&archive.dir) {
+        if let Err(e) = std::fs::create_dir(&archive.path) {
             error!("Failed to create archive directory {:?}: {}",
-                archive.dir.display(), e);
+                archive.path.display(), e);
             return Err(e);
         };
         if let Err(e) = archive.write_archive_header() {
             error!("Failed to write archive header: {}", e);
             return Err(e)
         };
-        info!("Created new archive in {:?}", dir.display());
+        info!("Created new archive in {:?}", path.display());
         Ok(archive)
     }
 
     /// Open an existing archive.
     ///
     /// Checks that the header is correct.
-    pub fn open(dir: &Path) -> Result<Archive> {
+    pub fn open(path: &Path) -> Result<Archive> {
         let archive = Archive {
-            dir: dir.to_path_buf(),
+            path: path.to_path_buf(),
         };
-        let header_path = dir.join(HEADER_FILENAME);
+        let header_path = path.join(HEADER_FILENAME);
         let mut header_file = match File::open(header_path.as_path()) {
             Ok(f) => f,
             Err(e) => {
@@ -94,7 +94,7 @@ impl Archive {
         let header = ArchiveHeader{
             conserve_archive_version: String::from(ARCHIVE_VERSION),
         };
-        let header_path = self.dir.join(HEADER_FILENAME);
+        let header_path = self.path.join(HEADER_FILENAME);
         let header_json = json::encode(&header).unwrap() + "\n";
         debug!("header json = {}", header_json);
         write_file_entire(&header_path, header_json.as_bytes())
@@ -102,17 +102,32 @@ impl Archive {
 
     /// Returns a vector of ids for bands currently present.
     pub fn list_bands(self: &Archive) -> Result<Vec<BandId>> {
-        // TODO: Not really implemented.
-        unimplemented!();
+        // TODO: Maybe make this an iterator?
+        let mut band_ids = Vec::<BandId>::new();
+        for entry_result in try!(read_dir(&self.path)) {
+            let entry = try!(entry_result);
+            if try!(entry.file_type()).is_dir() {
+                if let Ok(name_string) = entry.file_name().into_string() {
+                    if let Some(band_id) = BandId::from_string(&name_string) {
+                        band_ids.push(band_id);
+                    } else {
+                        warn!("unexpected archive subdirectory {:?}", &name_string);
+                    }
+                } else {
+                    warn!("unexpected archive subdirectory with un-decodable name {:?}",
+                        entry.file_name())
+                }
+            }
+        }
+        Ok(band_ids)
     }
-
 
     /// Returns the top-level directory for the archive.
     ///
     /// The top-level directory contains a `CONSERVE` header file, and zero or more
     /// band directories.
     pub fn path(self: &Archive) -> &Path {
-        self.dir.as_path()
+        self.path.as_path()
     }
 
     /// Make a new band. Bands are numbered sequentially.
@@ -147,49 +162,41 @@ mod tests {
     use std::io::Read;
 
     use super::*;
+    use super::super::band::BandId;
     use super::super::io::list_dir;
 
     #[test]
-    fn test_create_then_open_archive() {
+    fn create_then_open_archive() {
         let testdir = tempdir::TempDir::new("conserve-tests").unwrap();
         let arch_path = &testdir.path().join("arch");
         let arch = Archive::init(arch_path).unwrap();
 
         assert_eq!(arch.path(), arch_path.as_path());
+        assert!(arch.list_bands().unwrap().is_empty());
 
         // We can re-open it.
         Archive::open(arch_path).unwrap();
+        assert!(arch.list_bands().unwrap().is_empty());
     }
 
-    #[test]
-    fn test_new_archive_has_no_bands() {
-        // let (_tempdir, _arch) = scratch_archive();
-        // TODO: Implement list_bands
-        // assert!(arch.list_bands().unwrap().is_empty());
-    }
-
+    /// A new archive contains just one header file.
     /// The header is readable json containing only a version number.
     #[test]
-    fn test_archive_header_contents() {
+    fn new_archive_header_contents() {
         let (_tempdir, arch) = scratch_archive();
-        let mut header_path = arch.path().to_path_buf();
-        header_path.push("CONSERVE");
+
+        let (file_names, dir_names) = list_dir(arch.path()).unwrap();
+        assert_eq!(file_names.len(), 1);
+        assert!(file_names.contains("CONSERVE"));
+        assert_eq!(dir_names.len(), 0);
+
+        let header_path = arch.path().join("CONSERVE");
         let mut header_file = fs::File::open(&header_path).unwrap();
         let mut contents = String::new();
         header_file.read_to_string(&mut contents).unwrap();
         assert_eq!(
             contents,
             "{\"conserve_archive_version\":\"0.2.0\"}\n");
-    }
-
-    /// A new archive contains just one header file.
-    #[test]
-    fn new_archive_has_only_header() {
-        let (_tempdir, arch) = scratch_archive();
-        let (file_names, dir_names) = list_dir(arch.path()).unwrap();
-        assert_eq!(file_names.len(), 1);
-        assert!(file_names.contains("CONSERVE"));
-        assert_eq!(dir_names.len(), 0);
     }
 
     /// Can create bands in an archive.
@@ -204,6 +211,9 @@ mod tests {
         let (_file_names, dir_names) = list_dir(arch.path()).unwrap();
         println!("dirs: {:?}", dir_names);
         assert!(dir_names.contains("b0000"));
+
+        assert_eq!(arch.list_bands().unwrap(),
+            vec![BandId::new(&[0])]);
 
         // // Try creating a second band.
         // let _band2 = arch.create_band().unwrap();
