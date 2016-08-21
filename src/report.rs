7ec0f5e7034ece4df5e410caccffdcc3042a43d7
Martin Pool <mbp@sourcefrog.net>
2016-08-21 13:35:19 -0700

    All counters must be registered up front

--- src/report.rs
+++ src/report.rs
@@ -8,6 +8,7 @@ use std::collections;
 #[allow(unused)]
 static KNOWN_COUNTERS: &'static [&'static str] = &[
     "backup.file.count",
+    "backup.skipped.unsupported_file_kind",
     "block.read.count",
     "block.read.corrupt",
     "block.read.misplaced",
@@ -16,6 +17,7 @@ static KNOWN_COUNTERS: &'static [&'static str] = &[
     "block.write.count",
     "block.write.uncompressed_bytes",
     "index.write.compressed_bytes",
+    "index.write.uncompressed_bytes",
     "index.write.hunks",
     "source.selected.count",
     "source.skipped.unsupported_file_kind",
@@ -25,9 +27,7 @@ static KNOWN_COUNTERS: &'static [&'static str] = &[
 /// A Report is notified of problems or non-problematic events that occur while Conserve is
 /// running.
 ///
-/// A Report holds counters, identified by a name.  All implicitly start at 0.  All the
-/// counter
-/// names must be static strings.
+/// A Report holds counters, identified by a name.  The name must be in `KNOWN_COUNTERS`.
 #[derive(Clone, Debug)]
 pub struct Report {
     count: collections::HashMap<&'static str, u64>,
@@ -35,14 +35,23 @@ pub struct Report {
 
 impl Report {
     pub fn new() -> Report {
-        Report { count: collections::HashMap::new() }
+        let mut count = collections::HashMap::with_capacity(KNOWN_COUNTERS.len());
+        for counter_name in KNOWN_COUNTERS {
+            count.insert(*counter_name, 0);
+        }
+        Report { count: count }
     }
 
     /// Increment a counter by a given amount.
     ///
     /// The name must be a static string.  Counters implicitly start at 0.
     pub fn increment(self: &mut Report, counter_name: &'static str, delta: u64) {
-        *self.count.entry(counter_name).or_insert(0) += delta;
+        // Entries are created from the list of known names when this is constructed.
+        if let Some(mut c) = self.count.get_mut(counter_name) {
+            *c += delta;
+        } else {
+            panic!("unregistered counter {:?}", counter_name);
+        }
     }
 
     /// Return the value of a counter.  A counter that has not yet been updated is 0.
@@ -67,24 +76,24 @@ mod tests {
     #[test]
     pub fn count() {
         let mut r = Report::new();
-        assert_eq!(r.get_count("splines_reticulated"), 0);
-        r.increment("splines_reticulated", 1);
-        assert_eq!(r.get_count("splines_reticulated"), 1);
-        r.increment("splines_reticulated", 10);
-        assert_eq!(r.get_count("splines_reticulated"), 11);
+        assert_eq!(r.get_count("block.read.count"), 0);
+        r.increment("block.read.count", 1);
+        assert_eq!(r.get_count("block.read.count"), 1);
+        r.increment("block.read.count", 10);
+        assert_eq!(r.get_count("block.read.count"), 11);
     }
 
     #[test]
-    pub fn merge() {
+    pub fn merge_reports() {
         let mut r1 = Report::new();
         let mut r2 = Report::new();
-        r1.increment("a", 1);
-        r1.increment("common", 2);
-        r2.increment("inr2", 1);
-        r2.increment("common", 10);
+        r1.increment("block.read.count", 1);
+        r1.increment("block.read.corrupt", 2);
+        r2.increment("block.write.count", 1);
+        r2.increment("block.read.corrupt", 10);
         r1.merge_from(&r2);
-        assert_eq!(r1.get_count("a"), 1);
-        assert_eq!(r1.get_count("common"), 12);
-        assert_eq!(r1.get_count("inr2"), 1);
+        assert_eq!(r1.get_count("block.read.count"), 1);
+        assert_eq!(r1.get_count("block.read.corrupt"), 12);
+        assert_eq!(r1.get_count("block.write.count"), 1);
     }
 }
