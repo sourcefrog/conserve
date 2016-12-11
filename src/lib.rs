4825df601e47fb234bc3ccf97345539b24ea8cb2
Martin Pool <mbp@sourcefrog.net>
2016-12-11 11:12:12 -0800

    Use level 4 Brotli compression
    Helps with #32

--- src/lib.rs
+++ src/lib.rs
@@ -64,6 +64,6 @@ pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 /// Format-compatibility version, normally the first two components of the package version.
 const ARCHIVE_VERSION: &'static str = "0.3";
 
-const BROTLI_COMPRESSION_LEVEL: u32 = 9;
+const BROTLI_COMPRESSION_LEVEL: u32 = 4;
 
 pub const SYMLINKS_SUPPORTED: bool = cfg!(target_family="unix");
