1c8e7e75caf555cc03964612f17cfa359dff2ce1
Martin Pool <mbp@sourcefrog.net>
2016-12-18 09:32:34 -0800

    Add ui::by_name and basic tests

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -31,3 +31,39 @@ pub fn best_ui() -> Box<UI + Send> {
         Box::new(plain::PlainUI::new())
     }
 }
+
+
+/// Construct a UI by name.
+///
+/// `ui_name` may be `"auto"`, `"plain"`, or `"color"`.
+pub fn by_name(ui_name: &str) -> Option<Box<UI + Send>> {
+    match ui_name {
+        "auto" => Some(best_ui()),
+        "plain" => Some(Box::new(plain::PlainUI::new())),
+        "color" => match terminal::TermUI::new() {
+            Some(ui) => Some(Box::new(ui)),
+            None => None,
+        },
+        _ => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    pub fn best_ui() {
+        // You can at least construct some kind of UI although the type depends on the environment.
+        let _bestie = super::best_ui();
+    }
+
+    // TODO: Somehow test the type returned by `by_name`?
+    #[test]
+    pub fn by_name() {
+        // You must get some UI back from the default.
+        assert!(super::by_name("auto").is_some());
+        // Invalid names give None.
+        assert!(super::by_name("giraffe nativity").is_none());
+        // Plain UI should always be possible.
+        assert!(super::by_name("plain").is_some());
+    }
+}
