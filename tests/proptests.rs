7339771bcc774c70451b3049b2a0b86e1428d07d
Martin Pool <mbp@sourcefrog.net>
2018-10-19 23:59:43 -0400

    Further apath proptests

--- tests/proptests.rs
+++ tests/proptests.rs
@@ -7,8 +7,8 @@ extern crate conserve;
 use conserve::*;
 
 prop_compose! {
-    fn arb_single_apath(dot: bool, n in "[^/\0]+") {
-        "/" + if dot { "."} else { "" } + n
+    fn arb_single_apath()(dot: bool, n in "[^/.\0][^/\0]+") -> String {
+        "/".to_owned() + if dot { "."} else { "" } + &n
     }
 }
 
@@ -31,5 +31,27 @@ proptest! {
         } else {
             assert!(Apath::is_valid(a));
         }
+    }
+
+    #[test]
+    fn apath_simple_valid(ref a in arb_single_apath()) {
+        assert!(Apath::is_valid(a));
+    }
+
+    #[test]
+    fn apath_ordering(ref a in proptest::collection::vec(arb_single_apath(), 1..5),
+        ref b in proptest::collection::vec(arb_single_apath(), 1..5)) {
+        prop_assume!(a != b);
+        let a = a.join("");
+        let b = b.join("");
+        let aa = Apath::from(a.as_str());
+        let bb = Apath::from(b.as_str());
+        assert!((aa > bb && bb < aa) || (bb > aa && aa < bb));
+    }
+
+    #[test]
+    fn apath_long_valid(ref v in proptest::collection::vec(arb_single_apath(), 1..5)) {
+        let a = v.join("");
+        assert!(Apath::is_valid(&a), a);
     }
 }
