79970c0092ef1702b1b27e47b6e9d14441c8b3d0
Martin Pool <mbp@sourcefrog.net>
2018-10-19 22:04:50 -0400

    Crude proptest of apaths

--- tests/proptests.rs
+++ tests/proptests.rs
@@ -4,16 +4,26 @@
 extern crate proptest;
 
 extern crate conserve;
+use conserve::*;
 
 proptest! {
     #[test]
     fn parse_bandid(ref s in r"b[[:digit:]]{1,9}(-[[:digit:]]{1,9})*") {
-        let bid = conserve::BandId::from_string(s).unwrap();
-        let bid2 = conserve::BandId::from_string(s).unwrap();
+        let bid = BandId::from_string(s).unwrap();
+        let bid2 = BandId::from_string(s).unwrap();
         assert_eq!(bid, bid2);
         let bs = bid.as_string();
-        assert_eq!(conserve::BandId::from_string(bs).unwrap(), bid);
+        assert_eq!(BandId::from_string(bs).unwrap(), bid);
 
         assert!(bid.next_sibling() > bid);
+    }
+
+    #[test]
+    fn apath_valid(ref a in "(/|(/[^/\0]+)+)") {
+        if a.ends_with("/.") || a.ends_with("/..") || a.contains("/./") || a.contains("/../") {
+            assert!(!Apath::is_valid(a));
+        } else {
+            assert!(Apath::is_valid(a));
+        }
     }
 }
