a37e1978852779bd8bfdf6ae9d1d8da22eb84037
Martin Pool <mbp@sourcefrog.net>
2018-11-04 10:09:18 -0900

    Remove some use of spectral

--- src/excludes.rs
+++ src/excludes.rs
@@ -26,37 +26,36 @@ pub fn excludes_nothing() -> GlobSet {
 #[cfg(test)]
 mod tests {
     use super::super::*;
-    use spectral::prelude::*;
 
     #[test]
     pub fn simple_parse() {
         let vec = vec!["fo*", "foo", "bar*"];
         let excludes = excludes::from_strings(&vec).unwrap();
-        assert_that(&excludes.matches("foo").len()).is_equal_to(2);
-        assert_that(&excludes.matches("foobar").len()).is_equal_to(1);
-        assert_that(&excludes.matches("barBaz").len()).is_equal_to(1);
-        assert_that(&excludes.matches("bazBar").len()).is_equal_to(0);
+        assert_eq!(excludes.matches("foo").len(), 2);
+        assert_eq!(excludes.matches("foobar").len(), 1);
+        assert_eq!(excludes.matches("barBaz").len(), 1);
+        assert_eq!(excludes.matches("bazBar").len(), 0);
     }
 
     #[test]
     pub fn path_parse() {
         let excludes = excludes::from_strings(&["fo*/bar/baz*"]).unwrap();
-        assert_that(&excludes.matches("foo/bar/baz.rs").len()).is_equal_to(1);
+        assert_eq!(excludes.matches("foo/bar/baz.rs").len(), 1);
     }
 
     #[test]
     pub fn extendend_pattern_parse() {
         let excludes = excludes::from_strings(&["fo?", "ba[abc]", "[!a-z]"]).unwrap();
-        assert_that(&excludes.matches("foo").len()).is_equal_to(1);
-        assert_that(&excludes.matches("fo").len()).is_equal_to(0);
-        assert_that(&excludes.matches("baa").len()).is_equal_to(1);
-        assert_that(&excludes.matches("1").len()).is_equal_to(1);
-        assert_that(&excludes.matches("a").len()).is_equal_to(0);
+        assert_eq!(excludes.matches("foo").len(), 1);
+        assert_eq!(excludes.matches("fo").len(), 0);
+        assert_eq!(excludes.matches("baa").len(), 1);
+        assert_eq!(excludes.matches("1").len(), 1);
+        assert_eq!(excludes.matches("a").len(), 0);
     }
 
     #[test]
     pub fn nothing_parse() {
         let excludes = excludes::excludes_nothing();
-        assert_that(&excludes.matches("a").len()).is_equal_to(0)
+        assert!(excludes.matches("a").is_empty());
     }
 }
