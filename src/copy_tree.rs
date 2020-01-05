7803d3b29d78c2280ee59fc34abc527bdd17a915
Martin Pool <mbp@sourcefrog.net>
2020-01-05 13:58:50 -0800

    Don't measure the tree before copying
    Just start the real work as soon as possible.

--- src/copy_tree.rs
+++ src/copy_tree.rs
@@ -5,6 +5,8 @@
 
 use crate::*;
 
+const MEASURE_FIRST: bool = false;
+
 /// Copy files and other entries from one tree to another.
 ///
 /// Progress and problems are reported to the source's report.
@@ -13,12 +15,14 @@ pub fn copy_tree<ST: ReadTree, DT: WriteTree>(source: &ST, dest: &mut DT) -> Res
     // This causes us to walk the source tree twice, which is probably an acceptable option
     // since it's nice to see realistic overall progress. We could keep all the entries
     // in memory, and maybe we should, but it might get unreasonably big.
-    report.set_phase("Measure source tree");
-    // TODO: Maybe read all entries for the source tree in to memory now, rather than walking it
-    // again a second time? But, that'll potentially use memory proportional to tree size, which
-    // I'd like to avoid, and also perhaps make it more likely we grumble about files that were
-    // deleted or changed while this is running.
-    report.set_total_work(source.size()?.file_bytes);
+    if MEASURE_FIRST {
+        report.set_phase("Measure source tree");
+        // TODO: Maybe read all entries for the source tree in to memory now, rather than walking it
+        // again a second time? But, that'll potentially use memory proportional to tree size, which
+        // I'd like to avoid, and also perhaps make it more likely we grumble about files that were
+        // deleted or changed while this is running.
+        report.set_total_work(source.size()?.file_bytes);
+    }
     report.set_phase("Copying");
     for entry in source.iter_entries(&report)? {
         let entry = match entry {
