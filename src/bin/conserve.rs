7b6dbea444b8377c4963bdce328fbc77102904c6
Francesco Gadaleta <francesco@pryml.io>
2020-06-11 17:24:07 +0200

    make it Rusty and cleanup

--- src/bin/conserve.rs
+++ src/bin/conserve.rs
@@ -95,6 +95,7 @@ fn make_clap<'a, 'b>() -> clap::App<'a, 'b> {
             .multiple(true)
             .number_of_values(1)
             .value_name("GLOB")
+            .required(false)
             .help("Include only files that match the provided glob pattern")
     }
 
@@ -384,7 +385,8 @@ fn list_tree_contents<T: ReadTree>(tree: &T) -> Result<()> {
 
 fn restore(subm: &ArgMatches) -> Result<()> {
     let dest = Path::new(subm.value_of("destination").unwrap());
-    let only_path = Path::new(subm.value_of("only").unwrap());
+    // let only_path = Path::new(subm.value_of("only").unwrap());
+    let only_path = Path::new(subm.value_of("only").unwrap_or("/"));
 
     let st = stored_tree_from_options(subm)?;
     let rt = if subm.is_present("force-overwrite") {
--- src/copy_tree.rs
+++ src/copy_tree.rs
@@ -41,78 +41,70 @@ pub fn copy_tree<ST: ReadTree, DT: WriteTree>(
         ui::set_bytes_total(source.size()?.file_bytes);
     }
 
-    // Target selected for copy
+    // Target selected for copy (subpaths from here and included)
     let target = &options.restore_only;
     let target_tree: Vec<&str> = target.split('/').collect();
-    for (i, x) in target_tree.iter().enumerate() {
-        println!("\ntarget_tree {} -> {} [len: {:?}]", i, x, target_tree.len());
-    }
 
     ui::set_progress_phase("Copying");
     for entry in source.iter_entries()? {
         // Check if this entry is selected for copy
         let subtree: Vec<&str> = entry.apath().split('/').collect();
-        for (i, x) in subtree.iter().enumerate() {
-            println!("\nsubtree {} -> {} [len: {:?}]", i, x, subtree.len());
-        } 
-        
-        // println!("subtree is {:?}", subtree[1]);
         let mut to_be_copied: bool = false;
         
-        // FIXME take the top path from target and match it with entry (accept all subpaths)
-        if subtree.len() > target_tree.len() {
-        // if target_tree.len() == (subtree.len() - 1) {
-            // println!("Len matched");
-            let mut matched: usize = 0;
-            
-            for (i, _) in target_tree.iter().enumerate() {
-                if target_tree[i].eq(subtree[i+1]) {
-                    matched = matched + 1; 
-                    println!("target {:?}  subtree: {:?} matched: {:?}", 
-                    target_tree[i], subtree[i+1], matched);
-                    
+        match target.as_ref() {
+            "/" => to_be_copied = true,
+            _ => {
+                // Take the top path from target and match it with entry (accept all subpaths)
+                if subtree.len() > target_tree.len() {
+                    let mut matched: usize = 0;                    
+                    for (i, _) in target_tree.iter().enumerate() {
+                        if target_tree[i].eq(subtree[i+1]) {
+                            matched = matched + 1; 
+                        }
+                    }
+                    to_be_copied = matched == target_tree.len();
                 }
-                // println!("target_tree {} -> {} [len: {:?}]", i, x, target_tree.len());
             }
-            to_be_copied = matched == target_tree.len();
         }
 
-        // let to_be_copied = subtree[1].eq(target);
-        println!("to be copied: {:?}", to_be_copied);
-        
-        // FIXME rusty pls
-        if to_be_copied {
-            if options.print_filenames {
-                crate::ui::println(entry.apath());
+        match to_be_copied {
+            true => {
+                    if options.print_filenames {
+                        crate::ui::println(entry.apath());
+                    }
+                    ui::set_progress_file(entry.apath());
+                    if let Err(e) = match entry.kind() {
+                        Kind::Dir => {
+                            stats.directories += 1;
+                            dest.copy_dir(&entry)
+                        }
+                        Kind::File => {
+                            stats.files += 1;
+                            dest.copy_file(&entry, source).map(|s| stats += s)
+                        }
+                        Kind::Symlink => {
+                            stats.symlinks += 1;
+                            dest.copy_symlink(&entry)
+                        }
+                        Kind::Unknown => {
+                            stats.unknown_kind += 1;
+                            // TODO: Perhaps eventually we could backup and restore pipes,
+                            // sockets, etc. Or at least count them. For now, silently skip.
+                            // https://github.com/sourcefrog/conserve/issues/82
+                            continue;
+                        }
+                    } {
+                        ui::show_error(&e);
+                        stats.errors += 1;
+                        continue;
+                    }
+                    ui::increment_bytes_done(entry.size().unwrap_or(0));
+                }
+                _ => {
+                    // skip
+                }
             }
-            ui::set_progress_file(entry.apath());
-            if let Err(e) = match entry.kind() {
-                Kind::Dir => {
-                    stats.directories += 1;
-                    dest.copy_dir(&entry)
-                }
-                Kind::File => {
-                    stats.files += 1;
-                    dest.copy_file(&entry, source).map(|s| stats += s)
-                }
-                Kind::Symlink => {
-                    stats.symlinks += 1;
-                    dest.copy_symlink(&entry)
-                }
-                Kind::Unknown => {
-                    stats.unknown_kind += 1;
-                    // TODO: Perhaps eventually we could backup and restore pipes,
-                    // sockets, etc. Or at least count them. For now, silently skip.
-                    // https://github.com/sourcefrog/conserve/issues/82
-                    continue;
-                }
-            } {
-                ui::show_error(&e);
-                stats.errors += 1;
-                continue;
-            }
-            ui::increment_bytes_done(entry.size().unwrap_or(0));
-        }
+
     }
     ui::clear_progress();
     stats += dest.finish()?;
