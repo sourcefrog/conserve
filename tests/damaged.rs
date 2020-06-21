184df8b48540b7c7fb7b222bf7a0e6f5108e154d
Martin Pool <mbp@sourcefrog.net>
2020-06-21 10:38:58 -0700

    Test validating a damaged archive
    Add an archive with one missing block.
    
    Try to validate it and check that it's seen as missing.

---
+++ tests/damaged.rs
@@ -0,0 +1,28 @@
+// Conserve backup system.
+// Copyright 2020, Martin Pool.
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+//! Test validation of archives with some problems.
+
+use std::path::Path;
+
+use conserve::*;
+
+#[test]
+fn missing_block() -> Result<()> {
+    let archive = Archive::open_path(Path::new("testdata/damaged/missing-block"))?;
+
+    let validate_stats = archive.validate()?;
+    assert_eq!(validate_stats.has_problems(), true);
+    assert_eq!(validate_stats.block_missing_count, 1);
+    Ok(())
+}
