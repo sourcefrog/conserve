ac2ef792d0d35a4582903376eb7b3fb733884368
Martin Pool <mbp@sourcefrog.net>
2021-04-25 07:32:11 -0700

    More diff cli tests

--- tests/cli/diff.rs
+++ tests/cli/diff.rs
@@ -23,12 +23,6 @@ use crate::run_conserve;
 #[test]
 fn no_changes() {
     let (af, tf) = setup();
-    run_conserve()
-        .arg("backup")
-        .arg(af.path())
-        .arg(tf.path())
-        .assert()
-        .success();
 
     run_conserve()
         .arg("diff")
@@ -50,10 +44,57 @@ fn no_changes() {
         .stderr(predicate::str::is_empty());
 }
 
+#[test]
+fn add_entries() {
+    let (af, tf) = setup();
+    tf.create_dir("src");
+    tf.create_file_with_contents("src/new.rs", b"pub fn main() {}");
+
+    run_conserve()
+        .arg("diff")
+        .arg(af.path())
+        .arg(tf.path())
+        .assert()
+        .success()
+        .stdout("+\t/src\n+\t/src/new.rs\n")
+        .stderr(predicate::str::is_empty());
+}
+
+#[test]
+fn remove_file() {
+    let (af, tf) = setup();
+    std::fs::remove_file(tf.path().join("hello.c")).unwrap();
+
+    run_conserve()
+        .arg("diff")
+        .arg(af.path())
+        .arg(tf.path())
+        .assert()
+        .success()
+        .stdout("-\t/hello.c\n")
+        .stderr(predicate::str::is_empty());
+
+    run_conserve()
+        .arg("diff")
+        .arg("--include-unchanged")
+        .arg(af.path())
+        .arg(tf.path())
+        .assert()
+        .success()
+        .stdout(".\t/\n-\t/hello.c\n.\t/subdir\n")
+        .stderr(predicate::str::is_empty());
+}
+
 fn setup() -> (ScratchArchive, TreeFixture) {
     let af = ScratchArchive::new();
     let tf = TreeFixture::new();
     tf.create_file_with_contents("hello.c", b"void main() {}");
     tf.create_dir("subdir");
+    run_conserve()
+        .arg("backup")
+        .arg(af.path())
+        .arg(tf.path())
+        .assert()
+        .success();
     (af, tf)
 }
