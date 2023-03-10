0e3e3511b531cf41eb1fcc0ad3ee705755f646d5
Martin Pool <mbp@sourcefrog.net>
2023-03-09 20:04:23 -0800

    Test --changes-json from backup

--- tests/cli/backup.rs
+++ tests/cli/backup.rs
@@ -11,8 +11,12 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
+use std::fs::read_to_string;
+
 use assert_cmd::prelude::*;
+use assert_fs::NamedTempFile;
 use indoc::indoc;
+use serde_json::Deserializer;
 
 use conserve::test_fixtures::{ScratchArchive, TreeFixture};
 
@@ -25,17 +29,34 @@ fn backup_verbose() {
     src.create_dir("subdir");
     src.create_file("subdir/a");
     src.create_file("subdir/b");
+    let changes_json = NamedTempFile::new("changes.json").unwrap();
 
     run_conserve()
         .args(["backup", "--no-stats", "-v"])
         .arg(af.path())
         .arg(src.path())
+        .arg("--changes-json")
+        .arg(changes_json.path())
         .assert()
         .success()
         .stdout(indoc! { "
             + /subdir/a
             + /subdir/b
         "});
+
+    let changes_json = read_to_string(changes_json.path()).unwrap();
+    println!("{changes_json}");
+    let changes: Vec<serde_json::Value> = Deserializer::from_str(&changes_json)
+        .into_iter::<serde_json::Value>()
+        .map(Result::unwrap)
+        .collect();
+    assert_eq!(changes.len(), 2);
+    assert_eq!(changes[0]["apath"], "/subdir/a");
+    assert_eq!(changes[0]["change"], "Added");
+    assert_eq!(changes[0]["added"]["kind"], "File");
+    assert_eq!(changes[1]["apath"], "/subdir/b");
+    assert_eq!(changes[1]["change"], "Added");
+    assert_eq!(changes[1]["added"]["kind"], "File");
 }
 
 #[test]
