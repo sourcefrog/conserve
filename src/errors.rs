086a65c2a11a4fda2336c96dbe3a5729e80015dc
Martin Pool <mbp@sourcefrog.net>
2023-08-19 16:21:05 -0700

    Don't really need serializable errors

--- src/errors.rs
+++ src/errors.rs
@@ -17,35 +17,14 @@ use std::borrow::Cow;
 use std::io;
 use std::path::PathBuf;
 
-use serde::{self, ser::SerializeStruct, Serialize, Serializer};
 use thiserror::Error;
 
 use crate::blockdir::Address;
 use crate::*;
 
-fn serialize_io_error<S>(err: &io::Error, s: S) -> std::result::Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    let mut t = s.serialize_struct("io::Error", 2)?;
-    t.serialize_field("os_error", &err.raw_os_error())?;
-    t.serialize_field("message", &err.to_string())?;
-    t.end()
-}
-
-fn serialize_generic_error<E, S>(err: &E, s: S) -> std::result::Result<S::Ok, S::Error>
-where
-    E: std::error::Error,
-    S: Serializer,
-{
-    let mut t = s.serialize_struct("Error", 1)?;
-    t.serialize_field("message", &err.to_string())?;
-    t.end()
-}
-
 /// Conserve specific error.
 #[non_exhaustive]
-#[derive(Debug, Error, Serialize)]
+#[derive(Debug, Error)]
 pub enum Error {
     #[error("Block file {hash:?} corrupt; actual hash {actual_hash:?}")]
     BlockCorrupt { hash: String, actual_hash: String },
@@ -64,36 +43,22 @@ pub enum Error {
     },
 
     #[error("Failed to write block {hash:?}")]
-    WriteBlock {
-        hash: String,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    WriteBlock { hash: String, source: io::Error },
 
     #[error("Failed to read block {hash:?}")]
-    ReadBlock {
-        hash: String,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    ReadBlock { hash: String, source: io::Error },
 
     #[error("Block {block_hash} is missing")]
     BlockMissing { block_hash: BlockHash },
 
     #[error("Failed to list block files")]
-    ListBlocks {
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    ListBlocks { source: io::Error },
 
     #[error("Not a Conserve archive")]
     NotAnArchive {},
 
     #[error("Failed to read archive header")]
-    ReadArchiveHeader {
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    ReadArchiveHeader { source: io::Error },
 
     #[error(
         "Archive version {:?} is not supported by Conserve {}",
@@ -131,25 +96,16 @@ pub enum Error {
     InvalidVersion { version: String },
 
     #[error("Failed to create band")]
-    CreateBand {
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    CreateBand { source: io::Error },
 
     #[error("Band {band_id} head file missing")]
     BandHeadMissing { band_id: BandId },
 
     #[error("Failed to create block directory")]
-    CreateBlockDir {
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    CreateBlockDir { source: io::Error },
 
     #[error("Failed to create archive directory")]
-    CreateArchiveDirectory {
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    CreateArchiveDirectory { source: io::Error },
 
     #[error("Band {} is incomplete", band_id)]
     BandIncomplete { band_id: BandId },
@@ -172,77 +128,47 @@ pub enum Error {
     #[error(transparent)]
     ParseGlob {
         #[from]
-        #[serde(serialize_with = "serialize_generic_error")]
         source: globset::Error,
     },
 
     #[error("Failed to write index hunk {:?}", path)]
-    WriteIndex {
-        path: String,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    WriteIndex { path: String, source: io::Error },
 
     #[error("Failed to read index hunk {:?}", path)]
-    ReadIndex {
-        path: String,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    ReadIndex { path: String, source: io::Error },
 
     #[error("Failed to serialize index")]
-    SerializeIndex {
-        #[serde(serialize_with = "serialize_generic_error")]
-        source: serde_json::Error,
-    },
+    SerializeIndex { source: serde_json::Error },
 
     #[error("Failed to deserialize index hunk {:?}", path)]
     DeserializeIndex {
         path: String,
-        #[serde(serialize_with = "serialize_generic_error")]
         source: serde_json::Error,
     },
 
     #[error("Failed to write metadata file {:?}", path)]
-    WriteMetadata {
-        path: String,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    WriteMetadata { path: String, source: io::Error },
 
     #[error("Failed to deserialize json from {:?}", path)]
     DeserializeJson {
         path: PathBuf,
-        #[serde(serialize_with = "serialize_generic_error")]
         source: serde_json::Error,
     },
 
     #[error("Failed to serialize json to {:?}", path)]
     SerializeJson {
         path: String,
-        #[serde(serialize_with = "serialize_generic_error")]
         source: serde_json::Error,
     },
 
     #[error("Metadata file not found: {:?}", path)]
-    MetadataNotFound {
-        path: String,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    MetadataNotFound { path: String, source: io::Error },
 
     #[error("Failed to list bands")]
-    ListBands {
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    ListBands { source: io::Error },
 
     #[error("Failed to read source file {:?}", path)]
-    ReadSourceFile {
-        path: PathBuf,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    ReadSourceFile { path: PathBuf, source: io::Error },
 
     #[error("Unsupported source file kind: {path:?}")]
     UnsupportedSourceKind { path: PathBuf },
@@ -251,39 +177,19 @@ pub enum Error {
     UnsupportedTargetEncoding { path: PathBuf },
 
     #[error("Failed to read source tree {:?}", path)]
-    ListSourceTree {
-        path: PathBuf,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    ListSourceTree { path: PathBuf, source: io::Error },
 
     #[error("Failed to store file {:?}", apath)]
-    StoreFile {
-        apath: Apath,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    StoreFile { apath: Apath, source: io::Error },
 
     #[error("Failed to restore {:?}", path)]
-    Restore {
-        path: PathBuf,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    Restore { path: PathBuf, source: io::Error },
 
     #[error("Failed to restore modification time on {:?}", path)]
-    RestoreModificationTime {
-        path: PathBuf,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    RestoreModificationTime { path: PathBuf, source: io::Error },
 
     #[error("Failed to delete band {}", band_id)]
-    BandDeletion {
-        band_id: BandId,
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-    },
+    BandDeletion { band_id: BandId, source: io::Error },
 
     #[error("Unsupported URL scheme {:?}", scheme)]
     UrlScheme { scheme: String },
@@ -291,7 +197,6 @@ pub enum Error {
     #[error("Failed to serialize object")]
     SerializeError {
         #[from]
-        #[serde(serialize_with = "serialize_generic_error")]
         source: serde_json::Error,
     },
 
@@ -302,21 +207,15 @@ pub enum Error {
     #[error(transparent)]
     IOError {
         #[from]
-        #[serde(serialize_with = "serialize_io_error")]
         source: io::Error,
     },
 
     #[error("Failed to set owner of {path:?}")]
-    SetOwner {
-        #[serde(serialize_with = "serialize_io_error")]
-        source: io::Error,
-        path: PathBuf,
-    },
+    SetOwner { source: io::Error, path: PathBuf },
 
     #[error(transparent)]
     SnapCompressionError {
         // TODO: Maybe say in which file, etc.
-        #[serde(serialize_with = "serialize_generic_error")]
         #[from]
         source: snap::Error,
     },
