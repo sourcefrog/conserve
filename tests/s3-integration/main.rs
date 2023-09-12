0c2f7f13787529ebb67a09a15e72e1beb96e84fb
Martin Pool <mbp@sourcefrog.net>
2023-09-12 12:42:21 -0600

    Decent s3 integration smoke test

--- tests/s3-integration/main.rs
+++ tests/s3-integration/main.rs
@@ -22,13 +22,16 @@
 use std::str::FromStr;
 
 use ::aws_config::AppName;
+use assert_cmd::Command;
 use aws_sdk_s3::types::{
     BucketLifecycleConfiguration, BucketLocationConstraint, CreateBucketConfiguration,
     ExpirationStatus, LifecycleExpiration, LifecycleRule, LifecycleRuleFilter,
 };
-use aws_sdk_s3::Config;
+use futures::stream::StreamExt;
+use indoc::indoc;
 use rand::Rng;
-use rstest::{fixture, rstest};
+use time::macros::format_description;
+use time::OffsetDateTime;
 use tokio::runtime::Runtime;
 
 struct TempBucket {
@@ -37,8 +40,6 @@ struct TempBucket {
     client: aws_sdk_s3::Client,
 }
 
-#[fixture]
-#[once]
 fn temp_bucket() -> TempBucket {
     let runtime = tokio::runtime::Builder::new_current_thread()
         .enable_all()
@@ -52,8 +53,11 @@ fn temp_bucket() -> TempBucket {
     .unwrap();
     let config = runtime.block_on(::aws_config::from_env().app_name(app_name).load());
     let bucket_name = format!(
-        "conserve-s3-integration-{:x}",
-        rand::thread_rng().gen::<u64>()
+        "conserve-s3-integration-{time}-{rand:x}",
+        time = OffsetDateTime::now_utc()
+            .format(format_description!("[year][month][day]-[hour][minute]"))
+            .expect("Format time"),
+        rand = rand::thread_rng().gen::<u64>()
     );
 
     let client = aws_sdk_s3::Client::new(&config);
@@ -104,5 +108,137 @@ fn temp_bucket() -> TempBucket {
     }
 }
 
-#[rstest]
-fn hello(temp_bucket: &TempBucket) {}
+impl TempBucket {
+    pub fn url(&self) -> String {
+        format!("s3://{}", self.bucket_name)
+    }
+}
+
+impl Drop for TempBucket {
+    fn drop(&mut self) {
+        println!("Delete bucket {}", self.bucket_name);
+        let mut paginator = self
+            .client
+            .list_objects_v2()
+            .bucket(&self.bucket_name)
+            .into_paginator()
+            .send();
+        while let Some(page) = self.runtime.block_on(paginator.next()) {
+            for object in page
+                .expect("List objects page")
+                .contents
+                .unwrap_or_default()
+            {
+                self.runtime
+                    .block_on(
+                        self.client
+                            .delete_object()
+                            .bucket(&self.bucket_name)
+                            .key(object.key.unwrap())
+                            .send(),
+                    )
+                    .expect("Delete object");
+            }
+        }
+        self.runtime
+            .block_on(self.client.delete_bucket().bucket(&self.bucket_name).send())
+            .expect("Delete bucket");
+    }
+}
+
+fn conserve() -> Command {
+    Command::cargo_bin("conserve").expect("locate conserve binary")
+}
+
+#[test]
+fn integration_test() {
+    let temp_bucket = temp_bucket(); // Delete on drop
+    let url = &temp_bucket.url().to_string();
+    println!("init {url}");
+    conserve().arg("init").arg(url).assert().success();
+
+    // There are no versions in an empty archive
+    println!("versions {url}");
+    conserve()
+        .arg("versions")
+        .arg(url)
+        .assert()
+        .success()
+        .stdout("")
+        .stderr("");
+
+    // An empty archive is valid
+    println!("validate {url}");
+    conserve().arg("validate").arg(url).assert().success();
+
+    // Make a backup
+    println!("backup {url}");
+    conserve()
+        .arg("backup")
+        .arg(url)
+        .arg("testdata/tree/minimal")
+        .assert()
+        .success();
+
+    // There is one version
+    println!("versions {url}");
+    conserve()
+        .arg("versions")
+        .arg("--short")
+        .arg(url)
+        .assert()
+        .success()
+        .stdout("b0000\n")
+        .stderr("");
+
+    // It's valid
+    println!("validate {url}");
+    conserve().arg("validate").arg(url).assert().success();
+
+    // Can list files in the backup
+    println!("ls {url}");
+    conserve()
+        .arg("ls")
+        .arg(url)
+        .assert()
+        .success()
+        .stdout(indoc! { "
+            /
+            /hello
+            /subdir
+            /subdir/subfile
+        "})
+        .stderr("");
+
+    let restore_dir = tempfile::tempdir().expect("Create tempdir");
+    println!("restore {url}");
+    conserve()
+        .arg("restore")
+        .arg(url)
+        .arg(restore_dir.path())
+        .assert()
+        .success();
+    // TODO: Compare contents
+
+    println!("delete from {url}");
+    conserve()
+        .arg("delete")
+        .arg(url)
+        .arg("-b")
+        .arg("b0000")
+        .assert()
+        .success();
+
+    println!("validate {url}");
+    conserve().arg("validate").arg(url).assert().success();
+
+    println!("versions {url}");
+    conserve()
+        .arg("versions")
+        .arg("--short")
+        .arg(url)
+        .assert()
+        .success()
+        .stdout("")
+        .stderr("");
+}
