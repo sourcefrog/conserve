3ae5b74631bea503d195d66e7cf4c1afb7dd01af
Martin Pool <mbp@sourcefrog.net>
2025-07-06 13:13:15 -0700

    Test expiry deletion

--- tests/delete.rs
+++ tests/delete.rs
@@ -1,4 +1,4 @@
-// Copyright 2015, 2016, 2017, 2019, 2020 Martin Pool.
+// Copyright 2015-2025 Martin Pool.
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,9 +12,13 @@
 
 //! Test deletion.
 
+use std::time::Duration;
+
 use conserve::monitor::test::TestMonitor;
 use conserve::test_fixtures::store_two_versions;
 use conserve::*;
+use tempfile::TempDir;
+use time::OffsetDateTime;
 
 #[tokio::test]
 async fn delete_all_bands() {
@@ -32,4 +36,42 @@ async fn delete_all_bands() {
 
     assert_eq!(stats.deleted_block_count, 2);
     assert_eq!(stats.deleted_band_count, 2);
+}
+
+#[tokio::test]
+async fn delete_expired() -> Result<()> {
+    let af = Archive::create_temp().await;
+    let srcdir = TempDir::new().unwrap();
+    let monitor = TestMonitor::arc();
+    let now = OffsetDateTime::now_utc();
+
+    let day = Duration::from_secs(24 * 3600);
+    let mut backup_options = BackupOptions {
+        override_start_time: Some(now - 20 * day),
+        ..Default::default()
+    };
+    let _stats = backup(&af, srcdir.path(), &backup_options, monitor.clone()).await?;
+
+    backup_options.override_start_time = Some(now - 10 * day);
+    let _stats = backup(&af, srcdir.path(), &backup_options, monitor.clone()).await?;
+
+    backup_options.override_start_time = Some(now - day);
+    let _stats = backup(&af, srcdir.path(), &backup_options, monitor.clone()).await?;
+
+    let delete_stats = af
+        .delete_bands(
+            &[],
+            &DeleteOptions {
+                expiry_days: Some(2),
+                ..Default::default()
+            },
+            monitor.clone(),
+        )
+        .await
+        .expect("delete_bands");
+
+    dbg!(&delete_stats);
+    assert_eq!(delete_stats.deleted_block_count, 0);
+    assert_eq!(delete_stats.deleted_band_count, 2);
+    Ok(())
 }
